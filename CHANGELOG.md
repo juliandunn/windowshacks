1da21464dbba76b5e87dd8a471b18fd364128609
Julian C. Dunn <jdunn@opscode.com>
2013-05-31 17:36:41 -0500

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for windowshacks
+
+This file is used to list changes made in each version of windowshacks.
+
+## 0.1.0:
+
+* Initial release of windowshacks
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/disable-ie-esc-for-admins.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: windowshacks
+# Recipe:: disable-ie-esc-for-admins
+#
+# Copyright 2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Thanks to http://gpyall.com/archives/disable-enable-the-internet-explorer-enhanced-security-configuration-for-admins-with-group-policy/
+
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' do
+  values [{
+    :name => 'IsInstalled',
+    :type => :dword,
+    :data => 0
+  }]
+  action :create
+end
